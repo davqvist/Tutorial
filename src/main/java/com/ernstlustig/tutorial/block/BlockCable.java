1b52c09167e31e6c6f84827e0a0145c23a3722ed
davqvist <dpenack@gmail.com>
2016-03-25 01:31:51 +0100

    Collision and Bounding Boxes

--- src/main/java/com/ernstlustig/tutorial/block/BlockCable.java
+++ src/main/java/com/ernstlustig/tutorial/block/BlockCable.java
@@ -7,11 +7,16 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class BlockCable extends BlockTutorial {
 
@@ -60,4 +65,49 @@ public class BlockCable extends BlockTutorial {
     protected BlockState createBlockState() {
         return new BlockState( this, new IProperty[] {NORTH, EAST, SOUTH, WEST, UP, DOWN} );
     }
+
+    public void setBlockBoundsBasedOnState( IBlockAccess worldIn, BlockPos pos ){
+        float xmin = this.canConnectTo( worldIn, pos.west() ) ? 0.0F : 0.3125F;
+        float xmax = this.canConnectTo( worldIn, pos.east() ) ? 1.0F : 0.6875F;
+        float ymin = this.canConnectTo( worldIn, pos.down() ) ? 0.0F : 0.3125F;
+        float ymax = this.canConnectTo( worldIn, pos.up() ) ? 1.0F : 0.6875F;
+        float zmin = this.canConnectTo( worldIn, pos.north() ) ? 0.0F : 0.3125F;
+        float zmax = this.canConnectTo( worldIn, pos.south() ) ? 1.0F : 0.6875F;
+
+        this.setBlockBounds( xmin, ymin, zmin, xmax, ymax, zmax );
+    }
+
+    public void addCollisionBoxesToList( World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List<AxisAlignedBB> list, Entity collidingEntity ){
+        float min = 0.3125F;
+        float max = 0.6875F;
+
+        this.setBlockBounds( min, min, min, max, max, max );
+        super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+
+        if( this.canConnectTo( worldIn, pos.west() ) ){
+            this.setBlockBounds( 0.0F, min, min, max, max, max );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+        if( this.canConnectTo( worldIn, pos.east() ) ){
+            this.setBlockBounds( min, min, min, 1.0F, max, max );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+        if( this.canConnectTo( worldIn, pos.down() ) ){
+            this.setBlockBounds( min, 0.0F, min, max, max, max );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+        if( this.canConnectTo( worldIn, pos.up() ) ){
+            this.setBlockBounds( min, min, min, max, 1.0F, max );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+        if( this.canConnectTo( worldIn, pos.north() ) ){
+            this.setBlockBounds( min, min, 0.0F, max, max, max );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+        if( this.canConnectTo( worldIn, pos.south() ) ){
+            this.setBlockBounds( min, min, min, max, max, 1.0F );
+            super.addCollisionBoxesToList( worldIn, pos, state, mask, list, collidingEntity );
+        }
+    }
+
 }
